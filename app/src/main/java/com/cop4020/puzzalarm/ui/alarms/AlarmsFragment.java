3ef169989413fb5c5c2994782a429582b7b3f717
DevinM6 <devin8821@gmail.com>
2020-03-07 19:24:29 -0500

    Now using Pair arraylist to store times and passing that list into recyclerview. also 12 hour time working in recyclerview.

--- app/src/main/java/com/cop4020/puzzalarm/ui/alarms/AlarmsFragment.java
+++ app/src/main/java/com/cop4020/puzzalarm/ui/alarms/AlarmsFragment.java
@@ -1,30 +1,20 @@
 package com.cop4020.puzzalarm.ui.alarms;
 
 import android.app.TimePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cop4020.puzzalarm.R;
-import com.cop4020.puzzalarm.RecyclerViewAdapter;
-import com.cop4020.puzzalarm.ui.alarms.AlarmsViewModel;
-import com.cop4020.puzzalarm.ui.timepicker.TimePicker;
-import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -32,8 +22,7 @@ import java.util.Calendar;
 public class AlarmsFragment extends Fragment {
 
     private AlarmsViewModel alarmViewModel;
-    private ArrayList<String> mNames = new ArrayList<>();
-    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private ArrayList<Pair <Integer,Integer> > times = new ArrayList<>();
 
 
 
@@ -46,17 +35,9 @@ public class AlarmsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_alarms, container, false);
 
         com.google.android.material.floatingactionbutton.FloatingActionButton fabbtn = root.findViewById(R.id.floating_action_button);
-        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
-        mNames.add("Havasu Falls");
-
-        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
-        mNames.add("Trondheim");
-
-        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-        mNames.add("Portugal");
-        RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.my_recycler_view);
+        RecyclerView recyclerView = root.findViewById(R.id.my_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(getContext(), mImageUrls, mNames);
+        final RecyclerViewAdapter adapter = new RecyclerViewAdapter(getContext(), times);
 
         recyclerView.setAdapter(adapter);
 
@@ -69,14 +50,15 @@ public class AlarmsFragment extends Fragment {
                 TimePicker frag = new TimePicker();
                // fragmentTransaction.replace(R.id.nav_host_fragment, frag);
               //  fragmentTransaction.commit();
-
-
               -------------- going to copy and paste into open game function later.
 */
                 TimePickerDialog.OnTimeSetListener myTimeListener = new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(android.widget.TimePicker view, int hourOfDay, int minute) {
-
+                       // String TAG = Integer.toString(hourOfDay);
+                        Log.d("ADebugTag", "Value: " + hourOfDay + ":" + minute);
+                        times.add(Pair.create(hourOfDay, minute));
+                        adapter.notifyDataSetChanged();
                     }
                 };
                 final Calendar myCalender = Calendar.getInstance();
@@ -86,6 +68,8 @@ public class AlarmsFragment extends Fragment {
                 timePickerDialog.setTitle("Set New Alarm:");
                 timePickerDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
                 timePickerDialog.show();
+
+
             }
         });
 
--- app/src/main/java/com/cop4020/puzzalarm/ui/alarms/RecyclerViewAdapter.java
+++ app/src/main/java/com/cop4020/puzzalarm/ui/alarms/RecyclerViewAdapter.java
@@ -1,7 +1,9 @@
-package com.cop4020.puzzalarm;
+package com.cop4020.puzzalarm.ui.alarms;
+
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,27 +13,21 @@ import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-
 import com.cop4020.puzzalarm.MainActivity;
 import com.cop4020.puzzalarm.R;
 
-import org.w3c.dom.Text;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
 
     private static final String TAG = "RecyclerViewAdapter";
-
-    private ArrayList<String> mImageNames = new ArrayList<>();
-    private ArrayList<String> mImages = new ArrayList<>();
+   // private ArrayList<String> mImages = new ArrayList<>();
+    private ArrayList<Pair <Integer,Integer> > alarmList;
     private Context mContext;
 
-    public RecyclerViewAdapter(Context context, ArrayList<String> imageNames, ArrayList<String> images ) {
-        mImageNames = images;
-        mImages = images;
+    public RecyclerViewAdapter(Context context, ArrayList<Pair <Integer,Integer>> times ) {
+        alarmList = times;
         mContext = context;
     }
 
@@ -45,55 +41,41 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
         Log.d(TAG, "onBindViewHolder: called.");
+        if(alarmList.get(position).first>12){
+            holder.alarmTime.setText("" + (alarmList.get(position).first-12) + ":" + alarmList.get(position).second);
+        }
+        else{
+            holder.alarmTime.setText("" + alarmList.get(position).first + ":" + alarmList.get(position).second);
 
-
-
-        holder.imageName.setText(mImageNames.get(position));
-
+        }
         holder.parentLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG, "onClick: clicked on: " + mImageNames.get(position));
+                Log.d(TAG, "onClick: clicked on: " + alarmList.get(position).first + alarmList.get(position).second);
 
-                Toast.makeText(mContext, mImageNames.get(position), Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, + alarmList.get(position).first + ":" + alarmList.get(position).second, Toast.LENGTH_SHORT).show();
 
                 Intent intent = new Intent(mContext, MainActivity.class);
-                intent.putExtra("image_url", mImages.get(position));
-                intent.putExtra("image_name", mImageNames.get(position));
-                mContext.startActivity(intent);
+             //   intent.putExtra("image_url", mImages.get(position));
+                intent.putExtra("Time", alarmList.get(position).first + alarmList.get(position).second);
+              //  mContext.startActivity(intent);
             }
         });
     }
 
     @Override
     public int getItemCount() {
-        return mImageNames.size();
+        return alarmList.size();
     }
-
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView imageName;
+        TextView alarmTime;
         RelativeLayout parentLayout;
-
         public ViewHolder(View itemView) {
             super(itemView);
-            imageName = itemView.findViewById(R.id.image_name);
+            alarmTime = itemView.findViewById(R.id.alarmTime);
             parentLayout = itemView.findViewById(R.id.parentLayout);
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
