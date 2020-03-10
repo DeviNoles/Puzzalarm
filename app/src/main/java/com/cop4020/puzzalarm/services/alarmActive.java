7a09709091f76e0c85695e09d275f748f1096e8a
DevinM6 <devin8821@gmail.com>
2020-03-10 14:42:55 -0400

    +,-,*,/ in math game

--- app/src/main/java/com/cop4020/puzzalarm/services/alarmActive.java
+++ app/src/main/java/com/cop4020/puzzalarm/services/alarmActive.java
@@ -18,6 +18,7 @@ import java.util.Random;
 
 public class alarmActive extends AppCompatActivity {
     Ringtone rt;
+    int answer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,21 +33,44 @@ public class alarmActive extends AppCompatActivity {
         setContentView(R.layout.math);
         TextView num1 = findViewById(R.id.num1);
         TextView num2 = findViewById(R.id.num2);
+        TextView sign= findViewById(R.id.sign);
+
         Random r = new Random();
-        num1.setText(String.valueOf(r.nextInt(100)));
+        num1.setText(String.valueOf(r.nextInt(100))+1);
         r = new Random();
-        num2.setText(String.valueOf(r.nextInt(100)));
+        num2.setText(String.valueOf(r.nextInt(100))+1);
+        int tnum1 = Integer.parseInt(num1.getText().toString());
+        int tnum2 = Integer.parseInt(num2.getText().toString());
+        r = new Random();
+        int signVal = r.nextInt(4)+1;
+        if (signVal==1){
+            sign.setText("+");
+            answer = tnum1+tnum2;
+        }
+        else if (signVal==2) {
+            sign.setText("-");
+            answer = tnum1-tnum2;
+        }
+        else if (signVal==3) {
+            sign.setText("*");
+            answer = tnum1*tnum2;
+        }
+        else if (signVal==4) {
+            sign.setText("/");
+            answer = tnum1/tnum2;
+        }
         Button submit = findViewById(R.id.submit);
+
+
+
+
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText userInput = findViewById(R.id.userInput);
-                TextView answerNum1 = findViewById(R.id.num1);
-                TextView answerNum2 = findViewById(R.id.num2);
-                int tnum1 = Integer.parseInt(answerNum1.getText().toString());
-                int tnum2 = Integer.parseInt(answerNum2.getText().toString());
                 int tuserInput = Integer.parseInt(userInput.getText().toString());
-                if(tuserInput == tnum1 * tnum2)
+                if(tuserInput == alarmActive.this.answer)
                 {
                     rt.stop();
 
