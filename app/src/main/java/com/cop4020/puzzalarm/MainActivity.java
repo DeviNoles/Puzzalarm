613eb76cb3299bb579f60d035053e91c6d39c5cd
DevinM6 <devin8821@gmail.com>
2020-04-07 14:44:45 -0400

    Removed hideous actionbar

--- app/src/main/java/com/cop4020/puzzalarm/MainActivity.java
+++ app/src/main/java/com/cop4020/puzzalarm/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_alarms)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
         navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
             @Override
