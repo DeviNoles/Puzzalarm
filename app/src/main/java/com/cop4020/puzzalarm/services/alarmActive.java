bb8cb5f68369c52e315cb14a11d4112f3b9b37c4
pyrokuna <31514534+pyrokuna@users.noreply.github.com>
2020-04-24 21:47:01 -0400

    Created Password Puzzle  -&-  Fleshed out Home Fragment to Choose Between Puzzles/Games
    -Password puzzle has been created and is functional
    -The home fragment now has a welcome screen and description , as well as a radio group to select between games
    -Selected game now persists and is recognized in alarmActive through the use of sharedPreferences.

--- app/src/main/java/com/cop4020/puzzalarm/services/alarmActive.java
+++ app/src/main/java/com/cop4020/puzzalarm/services/alarmActive.java
@@ -1,5 +1,7 @@
 package com.cop4020.puzzalarm.services;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -8,7 +10,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cop4020.puzzalarm.R;
 
@@ -20,7 +25,11 @@ import java.util.Random;
 public class alarmActive extends AppCompatActivity {
 
     Ringtone rt;
-    int answer;
+    private int math_answer;
+    private String pw_answer;
+
+    RadioButton radioButton;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +39,18 @@ public class alarmActive extends AppCompatActivity {
         Uri rm = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
         rt = RingtoneManager.getRingtone(getApplicationContext(), rm);
         rt.play();
-        mathGame();
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("PuzzlePref", Context.MODE_PRIVATE);
+        String nameOfRadButton = sharedPreferences.getString("selectedPuzzle", "");
+
+        // need to switch between depending on result of shared preferences
+        if (nameOfRadButton.equals("Math Puzzle"))
+            mathGame();
+        else if (nameOfRadButton.equals("Password Puzzle"))
+            passwordGame();
     }
 
+    // this function was created using the GeeksforGeeks method seen at: https://www.geeksforgeeks.org/quick-ways-to-check-for-prime-and-find-next-prime-in-java/
     static boolean isPrime(long n)
     {
         BigInteger b = new BigInteger(String.valueOf(n));
@@ -50,7 +68,7 @@ public class alarmActive extends AppCompatActivity {
             r = new Random();
             int tnum1  = r.nextInt(100) + 20;
             int tnum2  = r.nextInt(100) + 20;
-            answer = tnum1+tnum2;
+            math_answer = tnum1+tnum2;
             String tQuestion = tnum1 + " + " + tnum2;
             question.setText(tQuestion);
         }
@@ -61,7 +79,7 @@ public class alarmActive extends AppCompatActivity {
             do {
                 tnum2 = r.nextInt(100) + 20;
             } while (tnum2 > tnum1);
-            answer = tnum1-tnum2;
+            math_answer = tnum1-tnum2;
             String tQuestion = tnum1 + " - " + tnum2;
             question.setText(tQuestion);
         }
@@ -70,7 +88,7 @@ public class alarmActive extends AppCompatActivity {
             // limiting possible numbers to 10 for realistic expectations
             int tnum1  = r.nextInt(10) + 1;
             int tnum2  = r.nextInt(10) + 1;
-            answer = tnum1*tnum2;
+            math_answer = tnum1*tnum2;
             String tQuestion = tnum1 + " × " + tnum2;
             question.setText(tQuestion);
         }
@@ -86,7 +104,7 @@ public class alarmActive extends AppCompatActivity {
             do {
                 tnum2  = r.nextInt(100) + 1;
             } while ((tnum1 % tnum2) != 0 || tnum2 == 1 || tnum2 >= tnum1);
-            answer = tnum1/tnum2;
+            math_answer = tnum1/tnum2;
             String tQuestion = tnum1 + " ÷ " + tnum2;
             question.setText(tQuestion);
         }
@@ -97,7 +115,44 @@ public class alarmActive extends AppCompatActivity {
             public void onClick(View v) {
                 EditText userInput = findViewById(R.id.userInput);
                 int tuserInput = Integer.parseInt(userInput.getText().toString());
-                if(tuserInput == answer) {
+                if(tuserInput == math_answer) {
+                    rt.stop();
+                    finish();
+                }
+            }
+        });
+    }
+
+    static String getRandomString(int size) {
+
+        Random r = new Random();
+        char[] characterPool = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
+                'R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','a','b',
+                'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x',
+                'y','z'};
+
+        String res = "";
+        for (int i = 0; i < size; i++) {
+            res = res + characterPool[r.nextInt(characterPool.length)];
+        }
+
+        return res;
+    }
+
+    private void passwordGame() {
+        setContentView(R.layout.password);
+        TextView password = findViewById(R.id.password_text);
+
+        password.setText(getRandomString(18));
+        pw_answer = password.getText().toString();
+
+        Button submit = findViewById(R.id.pw_submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText userInput = findViewById(R.id.pw_userInput);
+                String tuserInput = userInput.getText().toString();
+                if(tuserInput.equals(pw_answer)) {
                     rt.stop();
                     finish();
                 }
